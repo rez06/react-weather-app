c4d0bb9b50e73ff2bbbc28c0f6f64ebc7b8d24a1
rez06 <pelobello.rezyl@gmail.com>
2023-05-01 23:46:05 +0800

    Added files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import { useState } from "react";
+import axios from "axios";
 import './App.css';
 
 function App() {
+  const [city, setCity] = useState("");
+  const [temperature, setTemperature] = useState(null);
+  const [description, setDescription] = useState("");
+  const [humidity, setHumidity] = useState("");
+  const [icon, setIcon] = useState("");
+  const handleCityChange = (e) => {
+    e.preventDefault();
+    setCity(e.target.value);
+  };
+
+  const handleWeatherSearch = () => {
+    axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=bd3bb6534458ba51b48c49f5155745b6&units=metric`
+      )
+      .then((response) => {
+        setTemperature(response.data.main.temp);
+        setDescription(response.data.weather[0].description);
+        setHumidity(response.data.main.humidity);
+        setIcon(response.data.weather[0].icon);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" value={city} onChange={handleCityChange} />
+      <button onClick={handleWeatherSearch}>Search</button>
+      {temperature && (
+        <ul className="temp-info">
+          <li>City Name: {city} </li>
+          <li>Temperature: {temperature}°C</li>
+          <li>Description: {description}</li>
+          <li>Humidity: {humidity}%</li>
+          <img
+            className="weather-icon"
+            src={`http://openweathermap.org/img/w/${icon}.png`}
+            alt="weather-icon"
+          />
+        </ul>
+      )}
+      <p className="coder">Coded by <a href='#'>Rezyl Pelobello</a></p>
     </div>
   );
 }
